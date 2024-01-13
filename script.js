76dc2c0a88f7500e4f84c79f51b70352f8246529
Kedar Sudhir Jasud <111269122+Itskedarjasud@users.noreply.github.com>
2024-01-13 13:00:49 +0530

    files updated

---
+++ script.js
@@ -0,0 +1,52 @@
+const inputText = document.querySelector("#input-text");
+const listContainer = document.querySelector("#list-container");
+let icon = document.querySelector("#icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src="images/sun.png";
+    }
+    else{
+        icon.src="images/moon.png"
+    }
+}
+
+function addTask(){
+    if(inputText.value === ''){
+        alert("Must write somthing, then add");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputText.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+   inputText.value='';
+   saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+        
+    }
+}, false)
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
+
+
